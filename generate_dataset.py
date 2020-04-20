5428b13d52aceaa2055a1a44e1e3aa8492e220ca
fynnmaarten <fynnmaarten.business@gmail.com>
2020-04-20 03:23:31 +0200

    more memory efficiency

--- generate_dataset.py
+++ generate_dataset.py
@@ -5,11 +5,8 @@ import random
 
 import convert
 
-def generate(max_size, preprocess=False ,shuffle=False):
+def generate(raw_data, max_size, preprocess=False ,shuffle=False):
     dir = {"1-0": 1, "1/2-1/2": 0, "0-1": -1}
-    f = open("data/raw_data.txt", "r")
-    raw_data = f.read()
-    f.close()
 
     raw_data = raw_data.split("\n")[5:]
     
@@ -17,9 +14,6 @@ def generate(max_size, preprocess=False ,shuffle=False):
         random.shuffle(raw_data)
     
     size = 0
-    data = []
-    labels = []
-
     for match in raw_data:
         if size >= max_size:
             break
@@ -44,18 +38,30 @@ def generate(max_size, preprocess=False ,shuffle=False):
             history.append(board.fen())
             
             if preprocess:
+<<<<<<< Updated upstream
+=======
+                bb = convert.bitboard(history)
+                l = dir[result]
+                yield bb, l
+            else:
+                yield history, result
+            """
+            if preprocess:
+>>>>>>> Stashed changes
                 data.append(convert.bitboard(history))
                 labels.append(dir[result])
             else:
                 data.append(history.copy())
                 labels.append(result)
-    
+            """
+    """
     if shuffle: 
         c = list(zip(data, labels))
         random.shuffle(c)
         data, labels = zip(*c)
 
     return data, labels
+    """
 
 def format_number(num):
     suffixes = ['', 'K', 'M', 'G', 'T', 'P']
@@ -66,25 +72,61 @@ def format_number(num):
         num /= 1000.0
     return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), suffixes[m])
 
-def write_files(data, labels, path, size, is_testset=False):
+def write_files(data_gen, path, size, is_testset=False):
     #writes data and labels
     
-    name = "%strain_data_%s" if not is_testset else "%stest_data_%s"
+    trainfilename = "%strain_data_%s" if not is_testset else "%stest_data_%s"
+    labelsfilename = "%strain_labels_%s" if not is_testset else "%stest_labels_%s"
     size = 0.25 * size if is_testset else size
-
-    with open(name  % (path, format_number(size)), 'w') as f:
-        for list in data:
-            for index, fen in enumerate(list):
-                if index < len(list) - 1:
-                    f.writelines("%s," % fen)
-                else:
-                    f.writelines("%s\n" % fen)
-        f.close()
     
-    name = "%strain_labels_%s" if not is_testset else "%stest_labels_%s"
-    with open(name % (path, format_number(size)), 'w') as f:
-        f.writelines("%s\n" % result for result in labels)
-        f.close()
+    datafile = open(trainfilename  % (path, format_number(size)), 'w')
+    labelsfile = open(labelsfilename % (path, format_number(size)), 'w') 
+    for data, labels in data_gen: 
+        for idx, fen in enumerate(data):
+            if idx < len(data) - 1:
+                datafile.writelines("%s," % fen)
+            else:
+                datafile.writelines("%s\n" % fen)
+        
+        labelsfile.writelines("%s\n" % label for label in labels)
+    
+    datafile.close()
+    labelsfile.close()
+
+def shuffle(paths, preprocessed=False):
+    assert isinstance(paths, list)
+    assert len(paths) == 2
+    assert isinstance(paths[0], tuple) and isinstance(paths[1], tuple)
+    #TODO
+    if preprocessed:
+        pass
+    else:
+        for path in paths[:1]:
+            datafile = open(path[0], "r+")
+            labelsfile = open(path[1], "r+")
+            
+            data = datafile.read()
+            labels = labelsfile.read()
+
+            datafile.truncate(0)
+            labelsfile.truncate(0)
+
+            data = data.split("\n")
+            labels = labels.split("\n")
+
+            c = list(zip(data, labels))
+            random.shuffle(c)
+            data, labels = zip(*c)
+            
+            for fen in data:
+                datafile.writelines("%s\n" % fen)
+
+            for label in labels:
+                labelsfile.writelines("%s\n" % label)
+
+            datafile.close()
+            labelsfile.close()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -99,23 +141,38 @@ if __name__ == "__main__":
     path = args.path
     generate_testset = args.generate_testset
     preprocess = args.preprocess
-    shuffle = args.shuffle
-
-    data, labels = generate(size, preprocess=preprocess, shuffle=shuffle)
+    bool_shuffle = args.shuffle
     
+    #shuffle([("data/train_data_100K", "data/train_labels_100K"), ()])
+    f = open("data/raw_data.txt", "r")
+    raw_data = f.read()
+    f.close()
+
+    data_gen = generate(raw_data, size, preprocess=preprocess, shuffle=bool_shuffle)
+    
+    #TODO preprocess with generator 
     if preprocess:
-        data, labels = np.array(data), np.array(labels)
         name = "%strainset_%s.npz" % (path, format_number(size))
-        np.savez(name, data, labels)
+        for data, labels in data_gen:
+            data, labels = np.array(data), np.array(labels)
+            np.savez(name, data, labels)
     else:
-        write_files(data, labels, path, size)
+        write_files(data_gen, path, size)
 
     if generate_testset:
-        data, labels = generate(size * 0.25, preprocess=preprocess, shuffle=shuffle)
+        data_gen = generate(raw_data, size * 0.25, preprocess=preprocess, shuffle=shuffle)
         
+        #TODO preprocess with generator
         if preprocess:
-            data, labels = np.array(data), np.array(labels)
             name = "%stestset_%s.npz" % (path, format_number(size))
-            np.savez(name, data, labels)
+            for data, labels in data_gen:
+                data, labels = np.array(data), np.array(labels)
+                np.savez(name, data, labels)
         else:
-            write_files(data, labels, path, size, True)
+            write_files(data_gen, path, size, is_testset=True)
+    #TODO 
+    if shuffle:
+        if preprocessed:
+            shuffle(["","","",""], preprocessed=True)
+        else:
+            shuffle(["","","",""])
